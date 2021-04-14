5a3d3ce258d5818af22a15a0fe21075f9c0aca17
shiori102 <shiori10296@yahoo.com>
2021-04-14 17:50:13 -0400

    second

--- js/site.js
+++ js/site.js
@@ -1,17 +1,33 @@
 function printNumbers() {
     let start = parseInt(document.getElementById("numOne").value);
     let end = parseInt(document.getElementById("numTwo").value);
-    let numbers = getRange(start, end);
+    let fizz = parseInt(document.getElementById("numThree").value);
+    let buzz = parseInt(document.getElementById("numFour").value);
+    let numbers = getRange(start, end, fizz, buzz);
     displayData(numbers);
 }
 
-function getRange(start, end) {
+function getRange(start, end, fizz, buzz) {
     let numberArray = [];
     //fizzbuzz things go here
-    //mortgage things go here
-    for (let index = start; index <= end; index++) {
-        numberArray.push(index);
+    //This generates an array containing either 'Fizz', 'Buzz', 'FizzBuzz', or a number
+    //This is the 'data' of FizzBuzz
+    //The array is passed to displayData where the markup is added to make it pretty
+    for (let i = start; i <= end; i++) {
+        if (i % fizz == 0 && i % buzz == 0) {
+            numberArray.push("FizzBuzz");
+        } else if (i % fizz == 0) {
+            numberArray.push("Fizz");
+        } else if (i % buzz == 0) {
+            numberArray.push("Buzz");
+        } else {
+            numberArray.push(`${i}`);
+        }
     }
+    //mortgage things go here
+    // for (let index = start; index <= end; index++) {
+    //     numberArray.push(index);
+    // }
 
     return numberArray;
 }
@@ -31,20 +47,23 @@ function displayData(numbers) {
         let cols = dataRow.querySelectorAll("td");
 
         for (let colIndex = 0; colIndex < cols.length; colIndex++) {
-            let value = numbers[i + colIndex];
-            if (typeof value === "undefined") {
-                value = "";
-            } else if (value % 2 == 0) {
-                cols[colIndex].classList.add("boldIt");
+            // let value = numbers[i + colIndex];
+            //we add the i value from the first for loop to the colIndex value of this for loop
+            //that allows us to go through our data one at a time
+            if (numbers[i + colIndex] == "FizzBuzz") {
+                cols[colIndex].classList.add("fizzBuzz");
+                cols[colIndex].textContent = "FizzBuzz";
+            } else if (numbers[i + colIndex] == "Fizz") {
+                cols[colIndex].classList.add("fizz");
+                cols[colIndex].textContent = "Fizz";
+            } else if (numbers[i + colIndex] == "Buzz") {
+                cols[colIndex].classList.add("buzz");
+                cols[colIndex].textContent = "Buzz";
+            } else {
+                cols[colIndex].textContent = numbers[i + colIndex];
             }
 
-
-            //td's use textContent to set content
-            cols[colIndex].textContent = value;
+            resultsBody.appendChild(dataRow);
         }
-        //
-        resultsBody.appendChild(dataRow);
     }
-
-
 }
